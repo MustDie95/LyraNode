9fb3aee1709c57edfbdc5f0af19b1ca7425f8d2c
Pavel Kurdyukov <mustdie95@mail.ru>
2020-02-06 12:46:59 +0300

    Updated for 1.2.4 configuration changes

--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-[[ ! -f $HOME/Lyra-CLI-testnet/poswallet.db ]] && dotnet /opt/lyra/cli/lyracli.dll --networkid testnet -p webapi -g poswallet
+[[ ! -f $HOME/.Lyra/Lyra-CLI-testnet/poswallet.db ]] && dotnet /opt/lyra/cli/lyracli.dll --networkid testnet -p webapi -g poswallet
+
+\cp /opt/lyra/node/protocol.testnet.json  /root/.Lyra/protocol.testnet.json
 
 dotnet /opt/lyra/node/Lyra.Node2.dll
