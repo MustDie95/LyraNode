d5c1c0d386626c67a12ba71b8c1eb4daa81b69d4
Pavel Kurdyukov <mustdie95@mail.ru>
2020-02-21 12:24:13 +0300

    startup.sh fix

--- startup.sh
+++ startup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-[[ -f $HOME/Lyra-CLI-testnet/poswallet.db ]] && cp $HOME/Lyra-CLI-testnet/poswallet.db $HOME/.Lyra/Lyra-CLI-testnet/poswallet.db
-
-[[ ! -f $HOME/.Lyra/Lyra-CLI-testnet/poswallet.db ]] && dotnet /opt/lyra/cli/lyracli.dll --networkid testnet -p webapi -g poswallet
+[[ -f $HOME/Lyra-CLI-testnet/poswallet.db ]] && cp $HOME/Lyra-CLI-testnet/poswallet.db $HOME/.Lyra/Lyra-CLI-testnet/poswallet.db || dotnet /opt/lyra/cli/lyracli.dll --networkid testnet -p webapi -g poswallet
 
 \cp /opt/lyra/node/protocol.testnet.json  /root/.Lyra/protocol.testnet.json
 
